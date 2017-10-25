ff81e27d76427aeb5a258595bba4d32deb91cc3b
Sebastian Cullbrand <cullbrand@gmail.com>
2017-10-25 14:19:30 +0200

    Friend class added.

--- FriendOrganizer/FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer/FriendOrganizer.Model/Friend.cs
@@ -1,8 +1,10 @@
-﻿using System;
-
-namespace FriendOrganizer.Model
+﻿namespace FriendOrganizer.Model
 {
-    public class Class1
+    public class Friend
     {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
     }
 }
