f946753766d09f398dfb4732775725726c19e2cd
Sebastian Cullbrand <cullbrand@gmail.com>
2017-10-26 17:03:33 +0200

    Databasemigration added.

--- FriendOrganizer/FriendOrganizer.Model/Friend.cs
+++ FriendOrganizer/FriendOrganizer.Model/Friend.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
 
 namespace FriendOrganizer.Model
 {
     public class Friend
     {
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
         public string FirstName { get; set; }
+
+        [StringLength(50)]
         public string LastName { get; set; }
+
+        [StringLength(50)]
         public string Email { get; set; }
     }
 }
